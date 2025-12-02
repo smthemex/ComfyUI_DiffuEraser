6715762b527c351b36b2dabbe6535704a47582f9
smthemex <138738845+smthemex@users.noreply.github.com>
2025-12-02 14:30:26 +0800

    Update diffueraser_node.py

--- diffueraser_node.py
+++ diffueraser_node.py
@@ -13,6 +13,7 @@ import folder_paths
 from .run_diffueraser import load_diffueraser,load_propainter
 from diffusers.hooks import apply_group_offloading
 import copy
+
 MAX_SEED = np.iinfo(np.int32).max
 current_node_path = os.path.dirname(os.path.abspath(__file__))
 device = "cuda" if torch.cuda.is_available() else "mps" if torch.backends.mps.is_available() else "cpu"
@@ -61,7 +62,7 @@ class DiffuEraser_Loader(io.ComfyNode):
             display_name="DiffuEraser_Loader",
             category="DiffuEraser",
             inputs=[
-                io.Combo.Input("sd15",options= ["none"] + folder_paths.get_filename_list("checkpoints") ),
+                io.Combo.Input("vae",options= ["none"] + folder_paths.get_filename_list("vae") ),
                 io.Combo.Input("lora",options= ["none"] + folder_paths.get_filename_list("loras") ),
             ],
             outputs=[
@@ -69,8 +70,8 @@ class DiffuEraser_Loader(io.ComfyNode):
                 ],
             )
     @classmethod
-    def execute(cls, sd15,lora) -> io.NodeOutput:
-        ckpt_path=folder_paths.get_full_path("checkpoints",sd15) if sd15!="none" else None
+    def execute(cls, vae,lora) -> io.NodeOutput:
+        ckpt_path=folder_paths.get_full_path("vae",vae) if vae!="none" else None
         pcm_lora_path=folder_paths.get_full_path("loras",lora) if lora!="none" else None
         #print("load lora model from:",pcm_lora_path)
         model=load_diffueraser(os.path.join(current_node_path,"sd15_repo"),DiffuEraser_weigths_path, ckpt_path,os.path.join(current_node_path,"libs/v1-inference.yaml"),pcm_lora_path,device)
@@ -106,7 +107,7 @@ class DiffuEraser_PreData(io.ComfyNode):
             print("*********** Use input video and repo to make masks **************")
             init_mask=image2masks(seg_repo,video_image)
         elif video_mask_image is not None:
-          
+
             if not  isinstance(video_mask_image,torch.Tensor):
                 raise "video_mask_image is not a normal comfyUI image tensor, need a shape like  b,h,w,c"
             else:
@@ -159,6 +160,7 @@ class Propainter_Sampler(io.ComfyNode):
     @classmethod
     def execute(cls, model,conditioning,fps,video_length,mask_dilation_iter,ref_stride,neighbor_length,subvideo_length) -> io.NodeOutput:
         
+
         model.to(device)
         conditioning["fps"]=fps
         conditioning["video_length"]=video_length
@@ -183,6 +185,7 @@ class DiffuEraser_Sampler(io.ComfyNode):
             category="DiffuEraser",
             inputs=[
                 io.Custom("DiffuEraser_Loader").Input("model"),
+                io.Conditioning.Input("positive"),
                 io.Conditioning.Input("conditioning"),
                 io.Int.Input("steps", default=2, min=1, max=1024,step=1,display_mode=io.NumberDisplay.number),
                 io.Int.Input("seed", default=0, min=0, max=MAX_SEED,display_mode=io.NumberDisplay.number),
@@ -197,13 +200,25 @@ class DiffuEraser_Sampler(io.ComfyNode):
                 ],
              )
     @classmethod
-    def execute(cls, model,conditioning,steps,seed,save_result_video,unet_group,brush_group,blended) -> io.NodeOutput:
+    def execute(cls, model,positive,conditioning,steps,seed,save_result_video,unet_group,brush_group,blended) -> io.NodeOutput:
+        # gc cf model
+        cf_models=mm.loaded_models()
+        try:
+            for pipe in cf_models:   
+                pipe.unpatch_model(device_to=torch.device("cpu"))
+                print(f"Unpatching models.{pipe}")
+        except: pass
+        mm.soft_empty_cache()
+        torch.cuda.empty_cache()
+        max_gpu_memory = torch.cuda.max_memory_allocated()
+        print(f"After Max GPU memory allocated: {max_gpu_memory / 1000 ** 3:.2f} GB")
+        
         max_img_size=1920
         model.to(device) 
         model.pipeline.enable_xformers_memory_efficient_attention()
         apply_group_offloading(model.pipeline.unet, onload_device=torch.device("cuda"), offload_type="block_level", num_blocks_per_group=unet_group)
         apply_group_offloading(model.pipeline.brushnet, onload_device=torch.device("cuda"), offload_type="block_level", num_blocks_per_group=brush_group)
-        image_list=model.forward( copy.deepcopy(conditioning["video_image"]), copy.deepcopy(conditioning["init_mask"]), copy.deepcopy(conditioning["prioris"]),folder_paths.get_output_directory(),load_videobypath=False,
+        image_list=model.forward( copy.deepcopy(conditioning["video_image"]), copy.deepcopy(conditioning["init_mask"]), copy.deepcopy(conditioning["prioris"]),folder_paths.get_output_directory(),positive,load_videobypath=False,
                                 max_img_size = max_img_size, video_length=conditioning["video_length"], mask_dilation_iter=conditioning["mask_dilation_iter"],seed=seed,blended=blended,
                                num_inference_steps=steps,fps=conditioning["fps"],img_size=(conditioning["width"],conditioning["height"]),if_save_video=save_result_video)
         
